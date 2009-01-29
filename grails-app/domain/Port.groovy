6ea716b1665550b1a4c5fdaf568c21131db75e3b
a03182 <a03182@de972d9a-1e07-6d36-bcce-dc0026caf446>
2009-01-29 21:59:09 +0000

    Added small comment.
    git-svn-id: svn://eaimgr/opt/eaimgr/utilities/trunk/DependencyManagement@40 de972d9a-1e07-6d36-bcce-dc0026caf446

--- grails-app/domain/Port.groovy
+++ grails-app/domain/Port.groovy
@@ -2,6 +2,7 @@ class Port {
     Domain domain
     int port_number
     String port_use_description     // Oracle, JMS, HTTP, etc.
+                                    // MQSeries, FTP, SFTP, etc.
 
     Date dateCreated
     Date lastUpdated
